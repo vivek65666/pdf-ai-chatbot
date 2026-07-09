dda53a8ed4119d7a2516a0e9ef4806490d09ae06
Vivek C Raj <167602319+vivek65666@users.noreply.github.com>
2026-07-09 18:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ if uploaded_file and process_button:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = text_splitter.split_documents(docs)
         
-       embeddings = GoogleGenerativeAIEmbeddings(model="text-embedding-004", google_api_key=api_key)
+       # Create Embeddings and Vector Store
+        embeddings = GoogleGenerativeAIEmbeddings(model="text-embedding-004", google_api_key=api_key)
         st.session_state.vector_store = FAISS.from_documents(splits, embeddings)
         
         # Clean up temp file
